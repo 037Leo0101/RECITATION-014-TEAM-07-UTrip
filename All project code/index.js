df5d9519b8a9c5d021c34ec0995b341e7beb538c
Leo Su <iksu4562@colorado.edu>
2023-04-13 12:57:25 -0600

    default test case added

--- All project code/index.js
+++ All project code/index.js
@@ -63,6 +63,10 @@ app.use(
 
 // TODO - Include your API routes here
 
+app.get('/welcome', (req, res) => {
+    res.json({status: 'success', message: 'Welcome!'});
+  });
+
 app.get('/', (req, res) => {
     res.redirect('/login'); //this will call the /login route in the API
 });
@@ -162,5 +166,5 @@ app.get("/logout", (req, res) => {
 // <!-- Section 5 : Start Server-->
 // *****************************************************
 // starting the server and keeping the connection open to listen for more requests
-app.listen(3000);
+module.exports = app.listen(3000)
 console.log('Server is listening on port 3000');
