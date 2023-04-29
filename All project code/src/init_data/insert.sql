e7f84e56940b4606a33ded3cf5014b54640f42da
Matthew Kind <malfoy970@gmail.com>
2023-04-29 16:37:53 -0600

    All SQL functionalities work on initial inspection

--- All project code/src/init_data/insert.sql
+++ All project code/src/init_data/insert.sql
@@ -1,1 +1,7 @@
-INSERT INTO users (username, password) VALUES ('testuser', '$2a$10$bj2gvnUPPfE9J3VuLjQ3ZuJmD6/GkgMgMpx5oLWP/sl5hVwppHxqC');
+INSERT INTO users (username, password)
+VALUES ('testuser', '$2a$10$bj2gvnUPPfE9J3VuLjQ3ZuJmD6/GkgMgMpx5oLWP/sl5hVwppHxqC');
+
+/*INSERT INTO hotels (hotelName, hotelURL, username)
+VALUES ('Cool Place to Stay', 'abcdefg', 'testuser'),
+       ('Bad Place to Stay',  'hijklmn', 'testuser'),
+       ('Worst Place to Stay', 'opqrstu', 'eviluser');*/
